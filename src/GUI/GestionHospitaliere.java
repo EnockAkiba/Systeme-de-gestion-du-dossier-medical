bdf4053d2776f2691c6155fcecd8f41a2b0fa710
akiba1aw <akibawanjumbienock@gmail.com>
2021-09-10 13:58:00 +0100

    j'ajoute qlq etats de sortie

---
+++ src/GUI/GestionHospitaliere.java
@@ -0,0 +1,25 @@
+package GUI;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author akiba
+ */
+public class GestionHospitaliere  {
+static Bienvenue lo=new Bienvenue();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        Bienvenue F = new Bienvenue();
+//        F.setVisible(true);
+//        Login l = new Login();
+        lo.setVisible(true);
+    }
+    
+}
